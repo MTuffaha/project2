af4112b34c41e9928caaf4264f96321042edd373
MTuffaha <89648515+MTuffaha@users.noreply.github.com>
2023-12-07 18:36:26 -0500

    signup added and main.dart changed

---
+++ mobdevproj2/lib/signup_page.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+        backgroundColor: Color(0xFFFB4F4F),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: SignUpForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  String _email = '';
+  String _password = '';
+  String _confirmPassword = '';
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+     //firebase logic needed
+      print('Email: $_email');
+      print('Password: $_password');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your email';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              setState(() {
+                _email = value;
+              });
+            },
+          ),
+          SizedBox(height: 12.0),
+          TextFormField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a password';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              setState(() {
+                _password = value;
+              });
+            },
+          ),
+          SizedBox(height: 12.0),
+          TextFormField(
+            controller: _confirmPasswordController,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Confirm Password',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value != _passwordController.text) {
+                return 'Passwords do not match';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              setState(() {
+                _confirmPassword = value;
+              });
+            },
+          ),
+          SizedBox(height: 24.0),
+          ElevatedButton(
+            onPressed: _submitForm,
+            child: Text('Sign Up'),
+            style: ElevatedButton.styleFrom(
+              primary: Color(0xFFFBB3B3),
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
